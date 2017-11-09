2fbe8a9b04bb4e33ee9c906735fc221dbe7c9c0e
Zachst <33524272+Zachst@users.noreply.github.com>
2017-11-09 12:11:48 -0500

    Add files via upload

--- Mad lib_ZS.py
+++ Mad lib_ZS.py
@@ -1,3 +1,5 @@
+import time
+
 print("Write a boy's name")
 boy1 = input()
 
@@ -28,3 +30,7 @@ animal1 = input()
 ### MAD LIB ###
 
 print("Last winter " + boy1 + " went to " + place1 + " to eat some " + food1 + " When he was eating his " + food1 + " He slipped and fell and broke his " + bodypart + " He never recovered from the injury. Although he did not make it he was " + adjective1 + " to eat his " + food1 + " The next month he recovered from his injury and he went to play " + sport1 + " He fell and broke his " + bodypart2 + " He ended up being hospitalized for the rest of his life " + boy1 + " ran into a " + animal1 + " it ran up to " + boy1 + " and ran away")
+
+
+
+time.sleep(100)
