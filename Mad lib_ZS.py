5582f78607377170507cf72096ff1d7cd4c5a26d
Zachst <33524272+Zachst@users.noreply.github.com>
2017-11-16 12:34:53 -0500

    Changed adverb
    got rid of the hyphen between ad and verb because Mr.Rosenfeld said it wouldn't work.

--- Mad lib_ZS.py
+++ Mad lib_ZS.py
@@ -17,8 +17,8 @@ food1 = input ()
 print("write a bodypart")
 bodypart = input ()
 
-print("write a ad-verb")
-ad-verb1 = input()
+print("write a adverb")
+adverb1 = input()
 
 print("write a sport")
 sport1 = input()
@@ -29,7 +29,7 @@ print("write a animal")
 animal1 = input()
 ### MAD LIB ###
 
-print("Last winter " + boy1 + " went to " + place1 + " to eat some " + food1 + " When he was eating his " + food1 + " He slipped and fell and broke his " + bodypart + ". He never recovered from the injury. Although he did not make it he was " + ad-verb1 + " to eat his " + food1 + " The next month he recovered from his injury and he went to play " + sport1 + " He fell and broke his " + bodypart2 + " He ended up being hospitalized for the rest of his life " + boy1 + " ran into a " + animal1 + " it ran up to " + boy1 + " and he tried to run away, but it caught up and eat him.")
+print("Last winter " + boy1 + " went to " + place1 + " to eat some " + food1 + " When he was eating his " + food1 + " He slipped and fell and broke his " + bodypart + ". He never recovered from the injury. Although he did not make it he was " + adverb1 + " to eat his " + food1 + " The next month he recovered from his injury and he went to play " + sport1 + " He fell and broke his " + bodypart2 + " He ended up being hospitalized for the rest of his life " + boy1 + " ran into a " + animal1 + " it ran up to " + boy1 + " and he tried to run away, but it caught up and eat him.")
 
 
 
